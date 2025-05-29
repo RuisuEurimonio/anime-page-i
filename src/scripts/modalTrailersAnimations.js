6bbc17739d5748f0d3ff57a8ce60d8f2e5ba970b
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-05-29 17:44:31 -0500

    Se cambia logica de los eventos referente a las animaciones del modal de videos

---
+++ src/scripts/modalTrailersAnimations.js
@@ -0,0 +1,39 @@
+import {gsap} from "gsap";
+
+const duration1 = document.getElementById("animeDuration");
+const duration2 = document.getElementById("highDuration");
+const iframe = document.getElementById("iframe");
+const animePlay = document.getElementById("animePlay");
+const highPlay = document.getElementById("highPlay");
+
+export const animePlayAnimation = () =>{
+    if (duration1 && duration2 && iframe) {
+            duration1.textContent = "Reproduciendo";
+            duration2.textContent = "4:28"
+            iframe.src = "https://www.youtube.com/embed/_NXrTujMP50?si=Rmv3-LcXdHoDdCic"
+            gsap.to(animePlay,{
+                opacity: 0,
+                onComplete: ()=>{
+                    gsap.to(highPlay,{
+                        opacity: 1
+                    })
+                }
+            })
+        }
+}
+
+export const highPlayAnimation = () => {
+    if(duration1 && duration2 && iframe){
+            duration1.textContent = "4:21";
+            duration2.textContent = "Repoduciendo";
+            iframe.src = "https://www.youtube.com/embed/OEQPYC1Rvm4?si=n1lbFR1mXm9nQ9tU"
+            gsap.to(highPlay,{
+                opacity:0,
+                onComplete: ()=>{
+                    gsap.to(animePlay,{
+                        opacity:1
+                    })
+                }
+            })
+        }
+}
