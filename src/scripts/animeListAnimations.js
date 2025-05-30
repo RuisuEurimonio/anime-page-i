503e9a8bfe46820776f8b2b5d248cdd7478a1a38
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-05-30 18:54:19 -0500

    Se agrega el apartado de download al menu

--- src/scripts/animeListAnimations.js
+++
@@ -1,9 +0,0 @@
-import gsap from "gsap";
-
-export const toggleAnimesList = (animesList, isAnyOpen) => {
-    gsap.to(animesList,{
-        opacity: isAnyOpen ? 1 : 0,
-        display: isAnyOpen ? "block" : "none",
-        duration: 0.2
-      })
-}
--- src/scripts/sidebarAnimations.js
+++ src/scripts/sidebarAnimations.js
@@ -1,14 +1,12 @@
 import gsap from "gsap";
 
 export const toggleElementSidebar = (closeElement, isAnyOpen, openElement, closeAll = false) => {
-    console.log(isAnyOpen);
     gsap.to(closeElement,{
         opacity: isAnyOpen ? 1 : 0,
-        display: isAnyOpen ? "block" : "none",
+        display: isAnyOpen ? "flex" : "none",
         duration: 0.2,
         onComplete: closeAll ? ()=>{} : ()=>{
           if(openElement){
-            console.log("hla")
             toggleElementSidebar(openElement, true)
           }
         }
