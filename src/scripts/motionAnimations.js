a77e7e47043bd11063e8870532ff7c7917825e5a
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-05-29 19:21:07 -0500

    Se agrega animación al script de motion

--- src/scripts/motionAnimations.js
+++ src/scripts/motionAnimations.js
@@ -1,6 +1,10 @@
 import gsap from "gsap";
 
+const motionContainer = document.getElementById("motionContainer");
+const motionArrow = document.getElementById("motionArrow");
+
 let fullMovementActive = true;
+let openMotion = false;
 
 export const inputToggleQuestion = (movementInputBall) =>{
     gsap.to(movementInputBall,{
@@ -9,4 +13,21 @@ export const inputToggleQuestion = (movementInputBall) =>{
         ease: "power3.out"
       })
     fullMovementActive = !fullMovementActive;
+}
+
+export const toggleMotionContainer = () => {
+    gsap.to(motionContainer,{
+        opacity: openMotion ? 0 : 1,
+        zIndex: openMotion ? -10 : 30,
+        duration: 0.2
+      })
+    animationArrow();
+    openMotion = !openMotion;
+}
+
+const animationArrow = () => {
+    gsap.to(motionArrow,{
+        rotate: openMotion ? (window.innerWidth > 768 ? 180 : 360) : (window.innerWidth > 768 ? 360 : 180),
+        duration: 1
+      })
 }
