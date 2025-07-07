4f8103313da653027dc8b0b00c38b32a01cca82c
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-07-07 13:55:14 -0500

    Se mejora la animación del texto flotante

--- src/components/SailorMoonBottom.jsx
+++ src/components/SailorMoonBottom.jsx
@@ -28,6 +28,8 @@ const SailorMoonBottom = () => {
         setImages(imgs)
     },[])
 
+
+
     useEffect(()=>{
         if(!imgRef.current || images.length === 0) return;
         const obj = {frame : 0}
@@ -37,29 +39,41 @@ const SailorMoonBottom = () => {
                 start: "top top",
                 end: "bottom bottom",
                 scrub: true,
+            }
+        })
+
+        const tlAux = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#image-container_sailorBottom",
+                start: "top top",
+                endTrigger: "#title_sailorBottom",
+                end: "bottom+=100vh bottom",
+                scrub: true,
                 markers: true
             }
         })
 
-        tl.set("#title_sailorBottom",{
-            y: "-150vh",
-            opacity: 0
-        })
 
         tl.to("#image-container_sailorBottom",{
             opacity: 1,
             duration: 0.1
         })
 
-        tl.to("#title_sailorBottom",{
+        tlAux.to("#aux-title_sailorBottom",{
             opacity: 1,
+            y: "-8vh",
             duration: 0.2,
-            y: "-160vh"
+            delay: 0.05
         })
 
-        tl.to("#title_sailorBottom",{
-            duration: 1,
-            y: "0"
+        tlAux.to({},{duration: 0.70})
+
+        tlAux.set("#title_sailorBottom",{
+            opacity: 1
+        })
+
+        tlAux.set("#aux-title_sailorBottom",{
+            opacity: 0
         })
 
         tl.to(obj,{
@@ -84,6 +98,8 @@ const SailorMoonBottom = () => {
 
 
         return ()=>{
+            tlAux.scrollTrigger?.kill();
+            tlAux.kill();
             tl.scrollTrigger?.kill();
             tl.kill();
         }
@@ -93,13 +109,15 @@ const SailorMoonBottom = () => {
         <div className="relative">
             <div id="image-container_sailorBottom" className="h-[400vh] w-full absolute top-[-70vh] bg-[rgb(18,3,20)] z-0 opacity-0 overflow-y-visible">
                 <img ref={imgRef} src={getFramesSource(1)} className="sticky top-0 w-full h-screen object-cover" /> 
+                <h2 id="aux-title_sailorBottom" className="fixed text-5xl uppercase font-bold text-pink-500 bottom-8 ml-[calc(2.5rem)] pr-10 opacity-0
+                    md:text-6xl md:ml-[calc(12vw+2.5rem)] md:pr-0"> "Conejo de la Luna"  </h2>
             </div>
             <div id="text-container_sailorBottom" className="text-white z-10 relative pt-[220vh] px-10
                 sm:grid sm:grid-cols-[10vw_1.2fr_1.2fr_0.6fr] sm:grid-rows-[0.8fr_1fr_0.3fr_0.3fr] sm:gap-10 sm:px-0
-                md:grid-cols-[0.6fr_1.4fr_1.2fr_12vw] md:grid-rows-[0.4fr_1fr_5fr_3fr]
+                md:grid-cols-[12vw_1.4fr_1.2fr_12vw] md:grid-rows-[0.4fr_1fr_5fr_3fr]
             "> 
                 <div></div>
-                <h2 id="title_sailorBottom" className="text-5xl uppercase place-content-start font-bold text-pink-500 my-10 
+                <h2 id="title_sailorBottom" className="text-5xl uppercase place-content-start font-bold text-pink-500 my-10 opacity-0
                     md:text-6xl md:my-0 md:col-span-3 md:w-11/12
                 "> "Conejo de la Luna"  </h2>
 
