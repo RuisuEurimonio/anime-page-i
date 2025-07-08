4d6f82fdc6cf51f81f96ec48fffafe84347b5a47
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-07-08 11:33:57 -0500

    Se ajusta la forma de mostrar la imagen grande

--- src/components/ImgZoom.jsx
+++ src/components/ImgZoom.jsx
@@ -1,27 +1,31 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { gsap } from "../scripts/gsapConfig";
+import { createPortal } from "react-dom";
 
 const ImgZoom = ({imgSrc, ref, otherClass = ""}) => {
 
     const imgRef = useRef(null)
-    const overlayRef = useRef(null)
+
+    const [showFullImage, setShowFullImage] = useState(false);
 
     const handleClic = () => {
-        gsap.set(overlayRef.current, {display: "flex"})
+        setShowFullImage(true);
 
-        gsap.fromTo(imgRef.current,{
-            width: "8rem",
-            height: "8rem",
-            top: "50%",
-            left: "50%",
-            xPercent: -50,
-            yPercent: -50,
-        },
-        {
-            width: "90vw",
-            height: "90vh",
-            duration: 1,
-            ease: "power3.inOut",
+        requestAnimationFrame(()=>{
+            gsap.fromTo(imgRef.current,{
+                width: "8rem",
+                height: "8rem",
+                top: "50%",
+                left: "50%",
+                xPercent: -50,
+                yPercent: -50,
+            },
+            {
+                width: "90vw",
+                height: "90vh",
+                duration: 1,
+                ease: "power3.inOut",
+            })
         })
     }
 
@@ -32,20 +36,25 @@ const ImgZoom = ({imgSrc, ref, otherClass = ""}) => {
           duration: 1,
           ease: "power3.inOut",
           onComplete: () => {
-            gsap.set(overlayRef.current, { display: "none" });
+            setShowFullImage(false);
           },
         })
     }
 
     return(
         <>
-        <div ref={ref} className={"group w-full relative duration-200 hover:border-8 border-white cursor-zoom-in"+otherClass} onClick={handleClic}>
-            <img src={imgSrc} className="w-full h-full object-cover"/>
+        <div ref={ref} className={"group w-full relative duration-200 hover:border-8 border-white cursor-zoom-in "+otherClass} onClick={handleClic}>
+            <img src={imgSrc} loading="lazy" className="w-full h-full object-cover"/>
             <img src="/zoom.svg" className="group-hover:bg-pink-100 duration-200 group-hover:scale-125 absolute bottom-5 right-5 size-10 bg-pink-400 rounded-full p-2" />
         </div>
-        <div ref={overlayRef} className="fixed w-screen h-screen inset-0 z-50 bg-[rgba(18,3,20,0.9)] cursor-zoom-out hidden justify-center items-center" onClick={handleClose}>
-            <img ref={imgRef} src={imgSrc} className="absolute object-cover rounded-xl"/> 
-        </div>
+        {
+            showFullImage &&
+                createPortal(
+                <div className="fixed w-screen h-screen inset-0 z-50 bg-[rgba(18,3,20,0.9)] cursor-zoom-out justify-center items-center" onClick={handleClose}>
+                    <img ref={imgRef} src={imgSrc} className="absolute object-cover rounded-xl"/> 
+                </div>
+                ,document.body)
+        }
         </>
     )
 
