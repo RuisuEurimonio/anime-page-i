2fbe47fe13f6b1057481106eafd9bfa816a0b402
RuisuEurimonio <pipelinares2912@gmail.com>
2025-07-15 13:34:19 -0500

    Se agrega parte de zero two

--- src/components/zerotwo/ZeroTwo.jsx
+++ src/components/zerotwo/ZeroTwo.jsx
@@ -4,12 +4,12 @@ import ImgZoom from "../ImgZoom";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const frames = 41;
+const frames = 69;
 const getFramesSource = (index) => {
-    return `/frames/hinata/hinata_${index.toString()}.jpg`
+    return `/frames/zerotwo/zerotwo_${index.toString()}.jpg`
 }
 
-const Hinata = () => {
+const ZeroTwo = () => {
 
     const imgStatic = useRef(null);
     const containerRef = useRef(null);
@@ -21,9 +21,9 @@ const Hinata = () => {
 
     const obj = { frame: 0 };
 
-    gsap.timeline().set("#refreshVideoRef",{
+    gsap.timeline().set("#refreshVideoRef_zerotwo",{
         display: "none"
-    }).set("#pointVideoRef_hinata",{
+    }).set("#pointVideoRef_zerotwo",{
         display: "block"
     }).to(obj, {
             frame: frames - 1,
@@ -33,30 +33,30 @@ const Hinata = () => {
                 const index = Math.floor(obj.frame);
                 imgRef.current.src = images[index].src;
             }
-        }).fromTo("#pointVideoRef_hinata", {
+        }).fromTo("#pointVideoRef_zerotwo", {
             y: "100%",
         }, {
             y: "0%",
             duration: 3.5,
             ease: "power1.inOut"
-        }, "<").fromTo("#pointVideoContainerRef_hinata",{
+        }, "<").fromTo("#pointVideoContainerRef_zerotwo",{
             width: "1.25rem",
             height: "8rem"
         },{
             width: "3rem",
             height: "3rem",
             duration: 0.02
-        }).set("#pointVideoRef_hinata",{
+        }).set("#pointVideoRef_zerotwo",{
             display: "none",
-        }).fromTo("#pointVideoRef_hinata",{
+        }).fromTo("#pointVideoRef_zerotwo",{
             opacity: 1
         },{
             opacity: 0,
             duration: 0.01
-        },"<").set("#refreshVideoRef",{
+        },"<").set("#refreshVideoRef_zerotwo",{
             display: "flex",
         })
-        .fromTo("#refreshVideoRef",{
+        .fromTo("#refreshVideoRef_zerotwo",{
             opacity: 0
         },{
             opacity: 1,
@@ -90,7 +90,7 @@ const Hinata = () => {
     }, [])
 
     useEffect(() => {
-        ["#video-container_hinata", "#firstImages-container_hinata"].forEach((item, i) => {
+        ["#video-container_zerotwo", "#firstImages-container_zerotwo"].forEach((item, i) => {
             gsap.fromTo(item, {
                 opacity: i === 0 ? "" : 1,
                 filter: i === 0 ? "grayscale(100%)" : "",
@@ -99,7 +99,7 @@ const Hinata = () => {
                 filter: i === 0 ? "grayscale(0%)" : "",
                 duration: 1,
                 scrollTrigger: {
-                    trigger: "#firstImages-container_hinata",
+                    trigger: "#firstImages-container_zerotwo",
                     start: "center center",
                     end: "bottom top",
                     scrub: true,
@@ -110,7 +110,7 @@ const Hinata = () => {
     }, [])
 
     useEffect(()=>{
-        ["#secondImages-container_hinata", "#video-container_hinata"].forEach((item, i)=>{
+        ["#secondImages-container_zerotwo", "#video-container_zerotwo"].forEach((item, i)=>{
             gsap.fromTo(item,{
                 opacity: i === 0 ? 0 : "",
                 filter: i === 0 ? "" : "grayscale(0%)"
@@ -120,7 +120,7 @@ const Hinata = () => {
                 duration: 1,
                 ease: "power1.in",
                 scrollTrigger: {
-                    trigger: "#secondImages-container_hinata",
+                    trigger: "#secondImages-container_zerotwo",
                     start: "top bottom",
                     end: "center bottom",
                     scrub: true,
@@ -138,16 +138,16 @@ const Hinata = () => {
         const tl = gsap.timeline({
             
             scrollTrigger: {
-                trigger: "#image-container_hinata",
+                trigger: "#image-container_zerotwo",
                 start: "top top",
                 end: "bottom bottom",
                 scrub: true
             }
         })
 
-        tl.set("#refreshVideoRef",{
+        tl.set("#refreshVideoRef_zerotwo",{
         display: "none"
-    }).set("#pointVideoRef_hinata",{
+    }).set("#pointVideoRef_zerotwo",{
         display: "block"
     })
 
@@ -161,7 +161,7 @@ const Hinata = () => {
                     imgRef.current.src = images[index].src;
                 }
             }
-        }).fromTo("#pointVideoRef_hinata", {
+        }).fromTo("#pointVideoRef_zerotwo", {
             y: "100%",
             duration: 0.7
         }, {
@@ -169,24 +169,24 @@ const Hinata = () => {
             duration: 0.7
         }, "<")
         
-        tl.fromTo("#pointVideoContainerRef_hinata",{
+        tl.fromTo("#pointVideoContainerRef_zerotwo",{
             width: "1.25rem",
             height: "8rem"
         },{
             width: "3rem",
             height: "3rem",
             duration: 0.02
-        }).set("#pointVideoRef_hinata",{
+        }).set("#pointVideoRef_zerotwo",{
             display: "none",
-        }).fromTo("#pointVideoRef_hinata",{
+        }).fromTo("#pointVideoRef_zerotwo",{
             opacity: 1
         },{
             opacity: 0,
             duration: 0.01
-        },"<").set("#refreshVideoRef",{
+        },"<").set("#refreshVideoRef_zerotwo",{
             display: "flex",
         })
-        .fromTo("#refreshVideoRef",{
+        .fromTo("#refreshVideoRef_zerotwo",{
             opacity: 0
         },{
             opacity: 1,
@@ -204,7 +204,7 @@ const Hinata = () => {
         const tl = gsap.timeline({
             ease: "power1.inOut",
             scrollTrigger: {
-                trigger: "#image-parallax_hinata",
+                trigger: "#image-parallax_zerotwo",
                 start: "top center",
                 end: "bottom top",
                 scrub: true,
@@ -226,14 +226,14 @@ const Hinata = () => {
     }, [])
 
     return (
-        <div ref={containerRef} className="bg-[rgba(18,3,20,0)]">
-            <div id="image-parallax_hinata" className="w-full h-[140vh] overflow-y-hidden relative"
+        <div ref={containerRef} className="bg-[rgba(7,23,51,1)]">
+            <div id="image-parallax_zerotwo" className="w-full h-[140vh] overflow-y-hidden relative"
                 style={{ clipPath: "polygon(0% 10%, 100% 0%, 100% 100%, 0% 100%)" }}
             >
                 <div className="h-[160vh] w-full"
                     ref={imgStatic}
                     style={{
-                        backgroundImage: "url('/characters/hinata/hinata1.jpg')",
+                        backgroundImage: "url('/characters/zerotwo/zerotwo1.jpg')",
                         backgroundSize: "cover",
                         backgroundPosition: "center",
                     }}
@@ -243,32 +243,32 @@ const Hinata = () => {
                     ">
                         <h2 className="text-5xl text-fuchsia-400 uppercase text-right
                             md:text-7xl
-                        "> Hinata Hyuga </h2>
+                        "> Zero Two </h2>
                         <h3 className="text-2xl my-3 text-fuchsia-700 text-right
                             md:text-4xl
-                        "> Quiero ser más fuerte... como tú, Naruto. </h3>
+                        "> Si vas a matarme, hazlo después de besarme. </h3>
                         <p className="text-xl text-pink-500 text-right
                             md:text-3xl
-                        "> Tímida heredera Hyuga, encuentra inspiración en Naruto. Su amor y determinación la transforman en una valiente y poderosa ninja. </p>
+                        ">  Arma creada para destruir, que desafió su destino por amor y descubrió su humanidad junto a su darling.</p>
                     </div>
                 </div>
             </div>
-            <div id="firstImages-container_hinata" className="flex w-11/12 mx-auto my-5 flex-col gap-5 justify-center items-center
+            <div id="firstImages-container_zerotwo" className="flex w-11/12 mx-auto my-5 flex-col gap-5 justify-center items-center
                 sm:flex-row sm:h-[90vh] md:mt-24 md:gap-16 sm:ml-auto
             ">
-                <ImgZoom imgSrc="/characters/hinata/hinata2.jpeg" otherClass="h-8/12" alt="Hinata's second image" ></ImgZoom>
-                <ImgZoom imgSrc="/characters/hinata/hinata3.jpg" otherClass="h-full" alt="Hinata's third image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/zerotwo/zerotwo2.jpg" otherClass="h-8/12" alt="zerotwo's second image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/zerotwo/zerotwo3.jpg" otherClass="h-full" alt="zerotwo's third image" ></ImgZoom>
             </div>
-            <div id="image-container_hinata" className="h-[350vh] relative">
-                <div id="video-container_hinata" className="sticky top-0 h-[70vh] w-full grayscale
+            <div id="image-container_zerotwo" className="h-[350vh] relative">
+                <div id="video-container_zerotwo" className="sticky top-0 h-[70vh] w-full grayscale
                     sm:w-6/12 
                     md:h-[80vh] md:top-[10vh]
                 ">
                     <img ref={imgRef} src={getFramesSource(1)} className="w-full h-full object-cover cursor-pointer" onClick={handleClickVideo} />
 
-                    <span id="pointVideoContainerRef_hinata" className="w-5 h-32 rounded-full absolute bottom-5 right-0 mr-5 bg-gray-700 border-4 border-gray-700 flex justify-center overflow-hidden">
-                        <span id="pointVideoRef_hinata" className="w-2.5 block h-full translate-y-[95%] bg-white rounded-full"></span>
-                        <div className="hidden opacity-0 size-10 p-1.5 justify-center items-center" id="refreshVideoRef">
+                    <span id="pointVideoContainerRef_zerotwo" className="w-5 h-32 rounded-full absolute bottom-5 right-0 mr-5 bg-gray-700 border-4 border-gray-700 flex justify-center overflow-hidden">
+                        <span id="pointVideoRef_zerotwo" className="w-2.5 block h-full translate-y-[95%] bg-white rounded-full"></span>
+                        <div className="hidden opacity-0 size-10 p-1.5 justify-center items-center" id="refreshVideoRef_zerotwo">
                             <img  src="/refresh.svg" className="w-full h-full" />
                         </div>
                     </span>
@@ -276,29 +276,29 @@ const Hinata = () => {
                     
                     
                 </div>
-                <p id="textVideo_hinata" className="sticky top-[70vh] translate-y-1/2  font-bold text-fuchsia-400 text-4xl ml-auto
+                <p id="textVideo_zerotwo" className="sticky top-[70vh] translate-y-1/2  font-bold text-fuchsia-400 text-4xl ml-auto
                     sm:mb-[35vh] sm:right-6 sm:mr-20 sm:w-1/3
                     md:text-7xl
-                "> "No me rendiré... ¡Ese es mi camino ninja!" </p>
+                "> "Quería convertirme en humana... para poder estar contigo." </p>
             </div>
-            <div id="secondImages-container_hinata" className="flex  flex-col w-10/12 mx-auto mt-24 gap-16 justify-center items-center h-[90vh] pb-5
+            <div id="secondImages-container_zerotwo" className="flex  flex-col w-10/12 mx-auto mt-24 gap-16 justify-center items-center h-[90vh] pb-5
                 md:flex-row sm:pb-0
             ">
-                <ImgZoom imgSrc="/characters/hinata/hinata4.jpg" otherClass="h-11/12" alt="Hinata's fourth image" ></ImgZoom>
-                <ImgZoom imgSrc="/characters/hinata/hinata5.jpg" otherClass="h-8/12" alt="Hinata's fifth image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/zerotwo/zerotwo4.jpg" otherClass="h-11/12" alt="zerotwo's fourth image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/zerotwo/zerotwo5.jpg" otherClass="h-8/12" alt="zerotwo's fifth image" ></ImgZoom>
             </div>
             <div className="w-10/12 mx-auto font-bold pb-10
                 md:-translate-y-[30%] md:w-4/12 md:ml-auto md:mr-[10vw] 
             ">
                 <h2 className="text-2xl text-fuchsia-400
                     md:text-4xl
-                ">Fortaleza Silenciosa y Amor Inquebrantable</h2>
+                ">Hielo que aprendió a sentir</h2>
                 <p className="text-xl text-fuchsia-200
                     md:text-2xl
-                ">supera el rechazo y la inseguridad, inspirada por Naruto. Se convierte en una mujer valiente, amorosa y fuerte, formando con él una familia feliz.</p>
+                ">Guerrera creada para destruir que se eleva al cosmos, se sacrifica por amor y renace junto a Hiro, demostrando que su vínculo trasciende la muerte, el tiempo y el universo.</p>
             </div>
         </div>
     )
 }
 
-export default Hinata;
+export default ZeroTwo;
