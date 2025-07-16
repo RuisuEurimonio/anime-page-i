ec425a4743ecec3a4a23a0067e4382c6e445da86
RuisuEurimonio <pipelinares2912@gmail.com>
2025-07-16 15:28:48 -0500

    Se agrega componente de nao y se cambia texto en motoko

--- src/components/nao/Nao.jsx
+++ src/components/nao/Nao.jsx
@@ -4,12 +4,12 @@ import ImgZoom from "../ImgZoom";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const frames = 78;
+const frames = 100;
 const getFramesSource = (index) => {
-    return `/frames/motoko/motoko_${index.toString()}.jpg`
+    return `/frames/nao/nao_${index.toString()}.jpg`
 }
 
-const Motoko = () => {
+const Nao = () => {
 
     const imgStatic = useRef(null);
     const containerRef = useRef(null);
@@ -21,9 +21,9 @@ const Motoko = () => {
 
     const obj = { frame: 0 };
 
-    gsap.timeline().set("#refreshVideoRef_motoko",{
+    gsap.timeline().set("#refreshVideoRef_nao",{
         display: "none"
-    }).set("#pointVideoRef_motoko",{
+    }).set("#pointVideoRef_nao",{
         display: "block"
     }).to(obj, {
             frame: frames - 1,
@@ -33,30 +33,30 @@ const Motoko = () => {
                 const index = Math.floor(obj.frame);
                 imgRef.current.src = images[index].src;
             }
-        }).fromTo("#pointVideoRef_motoko", {
+        }).fromTo("#pointVideoRef_nao", {
             y: "100%",
         }, {
             y: "0%",
             duration: 3.5,
             ease: "power1.inOut"
-        }, "<").fromTo("#pointVideoContainerRef_motoko",{
+        }, "<").fromTo("#pointVideoContainerRef_nao",{
             width: "1.25rem",
             height: "8rem"
         },{
             width: "3rem",
             height: "3rem",
             duration: 0.02
-        }).set("#pointVideoRef_motoko",{
+        }).set("#pointVideoRef_nao",{
             display: "none",
-        }).fromTo("#pointVideoRef_motoko",{
+        }).fromTo("#pointVideoRef_nao",{
             opacity: 1
         },{
             opacity: 0,
             duration: 0.01
-        },"<").set("#refreshVideoRef_motoko",{
+        },"<").set("#refreshVideoRef_nao",{
             display: "flex",
         })
-        .fromTo("#refreshVideoRef_motoko",{
+        .fromTo("#refreshVideoRef_nao",{
             opacity: 0
         },{
             opacity: 1,
@@ -91,7 +91,7 @@ const Motoko = () => {
     }, [])
 
     useEffect(() => {
-        ["#video-container_motoko", "#firstImages-container_motoko"].forEach((item, i) => {
+        ["#video-container_nao", "#firstImages-container_nao"].forEach((item, i) => {
             gsap.fromTo(item, {
                 opacity: i === 0 ? "" : 1,
                 filter: i === 0 ? "grayscale(100%)" : "",
@@ -100,7 +100,7 @@ const Motoko = () => {
                 filter: i === 0 ? "grayscale(0%)" : "",
                 duration: 1,
                 scrollTrigger: {
-                    trigger: "#firstImages-container_motoko",
+                    trigger: "#firstImages-container_nao",
                     start: "center center",
                     end: "bottom top",
                     scrub: true,
@@ -111,7 +111,7 @@ const Motoko = () => {
     }, [])
 
     useEffect(()=>{
-        ["#secondImages-container_motoko", "#video-container_motoko"].forEach((item, i)=>{
+        ["#secondImages-container_nao", "#video-container_nao"].forEach((item, i)=>{
             gsap.fromTo(item,{
                 opacity: i === 0 ? 0 : "",
                 filter: i === 0 ? "" : "grayscale(0%)"
@@ -121,7 +121,7 @@ const Motoko = () => {
                 duration: 1,
                 ease: "power1.in",
                 scrollTrigger: {
-                    trigger: "#secondImages-container_motoko",
+                    trigger: "#secondImages-container_nao",
                     start: "top bottom",
                     end: "center bottom",
                     scrub: true,
@@ -139,16 +139,16 @@ const Motoko = () => {
         const tl = gsap.timeline({
             
             scrollTrigger: {
-                trigger: "#image-container_motoko",
+                trigger: "#image-container_nao",
                 start: "top top",
                 end: "bottom bottom",
                 scrub: true
             }
         })
 
-        tl.set("#refreshVideoRef_motoko",{
+        tl.set("#refreshVideoRef_nao",{
         display: "none"
-    }).set("#pointVideoRef_motoko",{
+    }).set("#pointVideoRef_nao",{
         display: "block"
     })
 
@@ -162,7 +162,7 @@ const Motoko = () => {
                     imgRef.current.src = images[index].src;
                 }
             }
-        }).fromTo("#pointVideoRef_motoko", {
+        }).fromTo("#pointVideoRef_nao", {
             y: "100%",
             duration: 0.7
         }, {
@@ -170,24 +170,24 @@ const Motoko = () => {
             duration: 0.7
         }, "<")
         
-        tl.fromTo("#pointVideoContainerRef_motoko",{
+        tl.fromTo("#pointVideoContainerRef_nao",{
             width: "1.25rem",
             height: "8rem"
         },{
             width: "3rem",
             height: "3rem",
             duration: 0.02
-        }).set("#pointVideoRef_motoko",{
+        }).set("#pointVideoRef_nao",{
             display: "none",
-        }).fromTo("#pointVideoRef_motoko",{
+        }).fromTo("#pointVideoRef_nao",{
             opacity: 1
         },{
             opacity: 0,
             duration: 0.01
-        },"<").set("#refreshVideoRef_motoko",{
+        },"<").set("#refreshVideoRef_nao",{
             display: "flex",
         })
-        .fromTo("#refreshVideoRef_motoko",{
+        .fromTo("#refreshVideoRef_nao",{
             opacity: 0
         },{
             opacity: 1,
@@ -205,7 +205,7 @@ const Motoko = () => {
         const tl = gsap.timeline({
             ease: "power1.inOut",
             scrollTrigger: {
-                trigger: "#image-parallax_motoko",
+                trigger: "#image-parallax_nao",
                 start: "top center",
                 end: "bottom top",
                 scrub: true,
@@ -228,13 +228,13 @@ const Motoko = () => {
 
     return (
         <div ref={containerRef} className="bg-[rgba(7,23,51,1)]">
-            <div id="image-parallax_motoko" className="w-full h-[140vh] overflow-y-hidden relative"
+            <div id="image-parallax_nao" className="w-full h-[140vh] overflow-y-hidden relative"
                 style={{ clipPath: "polygon(0% 10%, 100% 0%, 100% 100%, 0% 100%)" }}
             >
                 <div className="h-[160vh] w-full"
                     ref={imgStatic}
                     style={{
-                        backgroundImage: "url('/characters/motoko/motoko1.jpg')",
+                        backgroundImage: "url('/characters/nao/nao1.jpg')",
                         backgroundSize: "cover",
                         backgroundPosition: "center",
                     }}
@@ -244,32 +244,32 @@ const Motoko = () => {
                     ">
                         <h2 className="text-5xl text-fuchsia-400 uppercase text-right
                             md:text-7xl
-                        "> Motoko Kusanagi </h2>
+                        "> Nao Tomori </h2>
                         <h3 className="text-2xl my-3 text-fuchsia-700 text-right
                             md:text-4xl
-                        "> Estoy conectada a una vasta red. </h3>
+                        "> No existen los atajos para la vida. </h3>
                         <p className="text-xl text-pink-500 text-right
                             md:text-3xl
-                        ">  Una cyborg letal, comanda la Sección 9. Entre máquinas y humanidad, busca su identidad en un mundo digitalizado.</p>
+                        "> la chica que grababa verdades, protegía a quienes sufrían... y eligió quedarse, incluso cuando él la olvidó.</p>
                     </div>
                 </div>
             </div>
-            <div id="firstImages-container_motoko" className="flex w-11/12 mx-auto my-5 flex-col gap-5 justify-center items-center
+            <div id="firstImages-container_nao" className="flex w-11/12 mx-auto my-5 flex-col gap-5 justify-center items-center
                 sm:flex-row sm:h-[90vh] md:mt-24 md:gap-16 sm:ml-auto
             ">
-                <ImgZoom imgSrc="/characters/motoko/motoko2.jpg" otherClass="h-8/12" alt="motoko's second image" ></ImgZoom>
-                <ImgZoom imgSrc="/characters/motoko/motoko3.jpg" otherClass="h-full" alt="motoko's third image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/nao/nao2.jpg" otherClass="h-8/12" alt="nao's second image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/nao/nao3.jpg" otherClass="h-full" alt="nao's third image" ></ImgZoom>
             </div>
-            <div id="image-container_motoko" className="h-[350vh] relative">
-                <div id="video-container_motoko" className="sticky top-0 h-[70vh] w-full grayscale
+            <div id="image-container_nao" className="h-[350vh] relative">
+                <div id="video-container_nao" className="sticky top-0 h-[70vh] w-full grayscale
                     sm:w-6/12 
                     md:h-[80vh] md:top-[10vh]
                 ">
                     <img ref={imgRef} src={getFramesSource(1)} className="w-full h-full object-cover cursor-pointer" onClick={handleClickVideo} />
 
-                    <span id="pointVideoContainerRef_motoko" className="w-5 h-32 rounded-full absolute bottom-5 right-0 mr-5 bg-gray-700 border-4 border-gray-700 flex justify-center overflow-hidden">
-                        <span id="pointVideoRef_motoko" className="w-2.5 block h-full translate-y-[95%] bg-white rounded-full"></span>
-                        <div className="hidden opacity-0 size-10 p-1.5 justify-center items-center" id="refreshVideoRef_motoko">
+                    <span id="pointVideoContainerRef_nao" className="w-5 h-32 rounded-full absolute bottom-5 right-0 mr-5 bg-gray-700 border-4 border-gray-700 flex justify-center overflow-hidden">
+                        <span id="pointVideoRef_nao" className="w-2.5 block h-full translate-y-[95%] bg-white rounded-full"></span>
+                        <div className="hidden opacity-0 size-10 p-1.5 justify-center items-center" id="refreshVideoRef_nao">
                             <img  src="/refresh.svg" className="w-full h-full" />
                         </div>
                     </span>
@@ -277,29 +277,29 @@ const Motoko = () => {
                     
                     
                 </div>
-                <p id="textVideo_motoko" className="sticky top-[70vh] translate-y-1/2  font-bold text-fuchsia-400 text-4xl ml-auto
+                <p id="textVideo_nao" className="sticky top-[70vh] translate-y-1/2  font-bold text-fuchsia-400 text-4xl ml-auto
                     sm:mb-[35vh] sm:right-6 sm:mr-20 sm:w-1/3
                     md:text-7xl
-                "> "Me siento confinada, solo libre para expandirme dentro de límites." </p>
+                "> "Incluso si el mundo te odia, yo no lo haré." </p>
             </div>
-            <div id="secondImages-container_motoko" className="flex  flex-col w-10/12 mx-auto mt-24 gap-16 justify-center items-center h-[90vh] pb-5
+            <div id="secondImages-container_nao" className="flex  flex-col w-10/12 mx-auto mt-24 gap-16 justify-center items-center h-[90vh] pb-5
                 md:flex-row sm:pb-0
             ">
-                <ImgZoom imgSrc="/characters/motoko/motoko4.jpg" otherClass="h-11/12" alt="motoko's fourth image" ></ImgZoom>
-                <ImgZoom imgSrc="/characters/motoko/motoko5.jpg" otherClass="h-8/12" alt="motoko's fifth image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/nao/nao4.jpg" otherClass="h-11/12" alt="nao's fourth image" ></ImgZoom>
+                <ImgZoom imgSrc="/characters/nao/nao5.jpg" otherClass="h-8/12" alt="nao's fifth image" ></ImgZoom>
             </div>
             <div className="w-10/12 mx-auto font-bold pb-10
                 md:-translate-y-[30%] md:w-4/12 md:ml-auto md:mr-[10vw] 
             ">
                 <h2 className="text-2xl text-fuchsia-400
                     md:text-4xl
-                ">Tu esfuerzo por seguir siendo lo que eres es lo que te limita.</h2>
+                ">Lo grabé todo, así que no puedes mentir.</h2>
                 <p className="text-xl text-fuchsia-200
                     md:text-2xl
-                ">Al final se funde con la conciencia digital del enemigo que perseguía. Ya no es solo humana ni solo máquina: se transforma en algo nuevo, libre del sistema, trascendiendo el cuerpo y la identidad.</p>
+                "> En el olvido de él, Nao permaneció. Sin memoria, sin promesas, pero con fe: reconstruiría su historia, desde cero, una vez más. </p>
             </div>
         </div>
     )
 }
 
-export default Motoko;
+export default Nao;
