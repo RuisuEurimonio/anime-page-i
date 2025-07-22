687fe117beb324b87fd8fc4ff02171a9115711ae
RuisuEurimonio <pipelinares2912@gmail.com>
2025-07-21 21:08:41 -0500

    Se adelanta parte visual del componente

--- src/components/AnimeExpo.jsx
+++ src/components/AnimeExpo.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
+import { gsap } from "../scripts/gsapConfig";
 
 
 const AnimeExpo = ({}) => {
 
     const [listImages, setListImages] = useState([]);
+
+    const handleClickButton = () => {
+        document.body.style.overflow = "";
+        gsap.set("#animeInfo",{
+            display: "block"
+        })
+    }
 
     useEffect(()=>{
 
@@ -22,9 +30,26 @@ const AnimeExpo = ({}) => {
                 <button className=" absolute -bottom-16 right-0 left-0 mx-auto font-bold w-10/12 px-0.5 py-0.5 rounded-4xl bg-white
                     sm:py-1.5
                     md:py-2.5 md:bottom-5
-                "> Explorar Solo Leveling </button>
+                " onClick={handleClickButton}> Explorar Solo Leveling </button>
             </div>
-            <div className="absolute h-full overflow-x-scroll"> </div>
+            <div id="animeInfo" className="absolute h-full overflow-x-scroll hidden bg-[rgb(18,3,20)] z-[100]"> 
+                <div>
+                    <div>
+                        {mainImage}
+                        <h2> Solo leveling </h2>
+                        <h3> Sube de nivel </h3>
+                        <p> Solo Leveling sigue a Sung Jin-Woo, el cazador más débil del mundo, quien tras un evento misterioso obtiene la habilidad única de volverse más fuerte al completar misiones como en un videojuego. Su camino lo lleva de ser despreciado a convertirse en el cazador más poderoso.</p>
+                    </div>
+                    <div>
+                        <img src="" alt="" className="" />
+                        <img src="" alt="" className="" />
+                        <img src="" alt="" className="" />
+                    </div>
+                    {listImages.map((item)=>(
+                        <></>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
