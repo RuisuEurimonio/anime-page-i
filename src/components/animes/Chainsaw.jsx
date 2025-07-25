c94e43b610e835473a167bba296e7e62f21030d8
RuisuEurimonio <pipelinares2912@gmail.com>
2025-07-25 18:03:37 -0500

    Se agrega componente de Chainsaw

---
+++ src/components/animes/Chainsaw.jsx
@@ -0,0 +1,15 @@
+import AnimeExpo from "../AnimeExpo"
+
+const Chainsaw = () => {
+    return (
+        <AnimeExpo 
+            fullName={"Chainsaw Man"}
+            quote={"Quiero una vida normal, con pan, mermelada... y alguien que me abrace."}
+            text={"En Chainsaw Man, Denji es un chico que ha sufrido pobreza extrema y solo desea una vida sencilla. Al fusionarse con su demonio motosierra, es arrastrado a un mundo violento donde busca afecto y sentido, enfrentando la brutalidad del mundo demoníaco con un deseo muy humano."}
+            keyName={"chainsaw"}
+            listImagesProp={["chainsaw4","chainsaw5","chainsaw6"]}
+        />
+    )
+}
+
+export default Chainsaw;
