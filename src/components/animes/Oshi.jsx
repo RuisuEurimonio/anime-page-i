129381862ff2daa21eb37c468cb747998263878c
RuisuEurimonio <pipelinares2912@gmail.com>
2025-07-28 18:09:33 -0500

    Se agrega componente de oshi

---
+++ src/components/animes/Oshi.jsx
@@ -0,0 +1,16 @@
+import AnimeExpo from "../AnimeExpo"
+
+const Oshi = () => {
+
+    return (
+        <AnimeExpo
+            keyName={"oshi"}
+            fullName={"Oshi No Ko"}
+            quote={"Las mentiras son la mayor forma de amor."}
+            text={"Oshi no Ko es un anime que explora el lado oscuro del mundo del espectáculo japonés. Sigue la historia de un médico que reencarna como el hijo de su idol favorita tras su trágica muerte. A medida que crece, descubre secretos dolorosos y manipulaciones que rodean la industria del entretenimiento, mientras busca venganza y justicia."}
+            listImagesProp={["oshi4","oshi5","oshi6","oshi7"]}
+        />
+    )
+}
+
+export default Oshi;
