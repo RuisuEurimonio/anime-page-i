d6bb787607b5d714224ac6cdfbb1498ee57156bd
Ruisu Eurimonio <pipelinares2912@gmail.com>
2025-08-13 16:18:52 -0500

    Se agrega pantalla de carga

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+
+const LoadingScreen = () => {
+  const [loading, setLoading] = useState(true);
+  const loadingRef = useRef(null);
+
+  useEffect(() => {
+    const startAnimation = () => {
+      if (!loadingRef.current) return;
+
+      gsap.fromTo(
+        loadingRef.current,
+        { opacity: 1 },
+        {
+          opacity: 0,
+          duration: 2,
+          delay: 1.5,
+          ease: "power4.in",
+          onComplete: () => {
+            gsap.set(loadingRef.current, { display: "none" });
+            setLoading(false);
+          }
+        }
+      );
+    };
+
+    if (document.readyState === "complete") {
+      startAnimation();
+    } else {
+      window.addEventListener("load", startAnimation);
+      return () => window.removeEventListener("load", startAnimation);
+    }
+  }, []);
+
+  return (
+    <>
+      {loading && (
+        <section
+          ref={loadingRef}
+          className="fixed top-0 w-full h-screen z-[9999] bg-[radial-gradient(circle,rgba(18,3,20,1)_99%,rgba(51,20,74,1)_0%)]"
+        >
+          <div className="flex items-center justify-center w-full h-full">
+            <img src="/logoAnim.webp" alt="Logo de la página" className="size-32" />
+          </div>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default LoadingScreen;
